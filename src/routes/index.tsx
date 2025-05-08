921984132eb991155058612186d01dcc4ceccd5b
Parakrama Shashitha <t.m.parakrama@gmail.com>
2025-05-08 21:02:17 +0530

    fix: Spotify player not available when navigated to documentation pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { component$, useSignal, useOnWindow, $ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { BentoBox } from "~/components/ui/bento-box";
-import { SpotifyPlayer } from "~/components/spotify-player";
 
 export default component$(() => {
   const isLoaded = useSignal(false);
@@ -46,10 +45,7 @@ export default component$(() => {
           </div>
         </div>
       ) : (
-        <>
-          <BentoBox />
-          <SpotifyPlayer />
-        </>
+        <BentoBox />
       )}
     </>
   );
